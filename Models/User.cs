93c40254bdfa07caff7e299f5fb6bf786881e535
GDMeira <gabrieldega970@gmail.com>
2024-03-06 21:23:56 -0300

    feat: Models and DB creation. Maybe column UpdatedAt is not working properly.

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APIx.Models;
+
+public class User(string cpf, string name)
+{
+    public int Id { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    public string Cpf { get; set; } = cpf;
+
+    public string Name { get; set; } = name;
+
+    public ICollection<PaymentProviderAccount> PaymentProviderAccounts { get; set; } = null!;
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,22 @@
+using APIx.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Database
+builder.Services.AddDbContext<AppDBContext>(opts =>
+{
+    string host = builder.Configuration["Database:Host"] ?? string.Empty;
+    string port = builder.Configuration["Database:Port"] ?? string.Empty;
+    string username = builder.Configuration["Database:Username"] ?? string.Empty;
+    string database = builder.Configuration["Database:Name"] ?? string.Empty;
+    string password = builder.Configuration["Database:Password"] ?? string.Empty;
+
+    string connectionString = $"Host={host};Port={port};Username={username};Password={password};Database={database}";
+    opts.UseNpgsql(connectionString);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
