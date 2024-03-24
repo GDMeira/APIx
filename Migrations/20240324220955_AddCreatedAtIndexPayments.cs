f66521c1d4ea7f6d3907f0f6c3cb46157fa9a697
GDMeira <gabrieldega970@gmail.com>
2024-03-24 19:15:54 -0300

    feat: adding CreatedAt index on Payment table.

---
+++ Migrations/20240324220955_AddCreatedAtIndexPayments.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APIx.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreatedAtIndexPayments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Payment_CreatedAt",
+                table: "Payment",
+                column: "CreatedAt");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Payment_CreatedAt",
+                table: "Payment");
+        }
+    }
+}
